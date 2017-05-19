db4a43b1f7a17ffe37d272a418fadaf1da03e137
LQV <leewanvu@gmail.com>
2017-05-19 10:25:23 +0700

    update 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel demo
+                    Laravel demo 1
                 </div>
 
                 <div class="links">
